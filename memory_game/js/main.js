f513a40fe5c63f6cd8c1e97f94963586254d239e
Yevgeniy Drikman <purple@Yevgeniys-MacBook-Pro.local>
2019-10-04 07:25:39 +0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,36 @@
 console.log("Up and running!");
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
-console.log("User flipped " + cardOne);
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {	
+	if (cardsInPlay[0] === cardsInPlay[1]);
+	alert("You found a match!");
+	} else {
+	alert("Sorry, try again.");
+	}
+};
+
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	checkForMatch()
+}
+flipCard(0)
+flipCard(2)
+
+
+
+/*
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne)
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+*/
+
+
+
+
+
